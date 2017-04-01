626eb12e0b78085bc42b2899b1e78eab2d8fde2d
Petur Einarsson <pe301@itl116.student.eecs.qmul.ac.uk>
2017-04-01 15:38:44 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='PeturEinarsson',
+      version='1.0',
+      description='Petur Einarsson - Test app for Cloud computing',
+      author='Petur Einarsson',
+      author_email='contact@petur.uk',
+      url='https://www.python.org/community/sigs/current/distutils-sig',
+      install_requires=['flask','flask_navigation','flask-wtf','flask-babel',
+                        'markdown','flup', 'unirest', 'Flask-PyMongo', 'bokeh',
+                        'numpy' ]
+      )
---
+++ wsgi/app/database.py
@@ -0,0 +1,16 @@
+from app import app
+from flask_pymongo import PyMongo
+from flask import jsonify
+import os
+
+#app.config['MONGO_DBNAME'] = os.environ['pe301apitest1']
+
+app.config['MONGO_DBNAME'] = os.environ['OPENSHIFT_APP_NAME']
+app.config['MONGO_URI'] = os.environ['OPENSHIFT_MONGODB_DB_URL'] + os.environ['OPENSHIFT_APP_NAME']
+
+mongo = PyMongo(app)
+
+@app.route('/database/collections', methods=['GET'])
+def get_all_databases():
+	#return "hello"
+	return jsonify({'result' : mongo.db.collection_names()})
